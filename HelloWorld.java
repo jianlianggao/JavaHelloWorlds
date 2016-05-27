f03f912bf876132941a80dd537a68e300c1c6dad
jianlianggao <j.gao@imperial.ac.uk>
2016-05-27 14:47:24 +0100

    update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String args[]){
         //System.out.println("Hello World .. Jenkins is here!");
-        System.out.println("Noureddin Sadawi");
+        System.out.println("Jenkins test 1 -- Jianliang Gao");
     }
 }
