2be3f9df9aedd5cc1d4f7a7f584a4c603ca07f31
jianlianggao <b1.wong@yahoo.com>
2016-06-02 18:16:14 +0100

    add one new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String args[]){
         //System.out.println("Hello World .. Jenkins is here!");
         System.out.println("Jenkins test 1 -- Jianliang Gao");
+        System.out.println("Jenkins update 1 -- 2 June 2016"); 
     }
 }
