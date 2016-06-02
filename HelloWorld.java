0ec5b9060313f4088a1601ba995736f04ad78a31
jianlianggao <b1.wong@yahoo.com>
2016-06-02 20:30:25 +0100

    add one more new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Jenkins update 1 -- 2 June 2016"); 
         System.out.println("Jenkins update 2 -- 2 June 2016");
          System.out.println("Jenkins update 3 -- 2 June 2016");
+        System.out.println("Jenkins update 4 -- 2 June 2016");
     }
 }
